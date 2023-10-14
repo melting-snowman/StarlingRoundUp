cd7f05a2bae68cdbbc824b77a86b8381245b7142
Alex Ioja-Yang <alexiojayang@gmail.com>
2023-10-14 15:11:20 +0100

    Commit initial new project

---
+++ StarlingTechnicalChallenge/AccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarlingTechnicalChallenge
+//
+//  Created by Alex Ioja-Yang on 14/10/2023.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StarlingTechnicalChallenge/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  StarlingTechnicalChallenge
+//
+//  Created by Alex Ioja-Yang on 14/10/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+
+}
+
